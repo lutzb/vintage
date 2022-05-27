23733aaaf593b5c78e19a49b2f118dda8a3f4485
Bryan Lutz <bryanlutz.212@gmail.com>
2022-05-26 22:44:14 -0400

    Add video from vimeo embed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Home.css';
 import HomeHeader from '../HomeHeader/HomeHeader'
+import HomeBody from '../HomeBody/HomeBody'
 import Footer from '../Footer/Footer'
 
 function Home() {
   return (
     <div className="home">
       <HomeHeader />
+      <HomeBody />
       <Footer />
     </div>
   );
---
+++ src/components/HomeBody/HomeBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomeBody.scss'
+import ProBeachHockey from '../../img/pro-beach-hockey.gif'
+
+const HomeBody = () => (
+  <div className="home-header">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/707609150?h=5178318fd7" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+  </div>
+)
+
+export default HomeBody
