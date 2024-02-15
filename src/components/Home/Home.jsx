33dc148f955529b21052df4682702828074742e0
Bryan Lutz <bryanlutz.212@gmail.com>
2024-02-15 09:22:08 -0700

    Add poster and adjust logos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.scss';
 import Banner from '../Banner/Banner'
 import NavMenu from '../NavMenu/NavMenu'
+import PBHPoster from '../../img/pbh-poster.jpg'
 import Team1 from '../../img/team-dawgpac-logo.png'
 import Team2 from '../../img/team-express-logo.png'
 import Team3 from '../../img/team-gargoyles-logo.png'
@@ -17,22 +18,31 @@ function Home() {
 
       {/* HOME THINGS */}
       <div className="home-body">
-        {/* Show top images */}
-        <div className="team-images">
-          <img className="team-image-logo" src={Team1} alt=''/>
-          <img className="team-image-logo" src={Team2} alt=''/>
-          <img className="team-image-logo" src={Team3} alt=''/>
+        {/* Show left images */}
+        <div className="home-team-images">
+          <img className="home-team-image-logo" src={Team1} alt=''/>
+          <img className="home-team-image-logo" src={Team2} alt=''/>
+          <img className="home-team-image-logo" src={Team3} alt=''/>
         </div>
 
-        {/* Show video */}
-        <iframe className="video-player" title="vimeo-player" src="https://player.vimeo.com/video/707609150?h=5178318fd7" frameBorder="0" allowFullScreen></iframe>
+        {/* Show video / poster */}
+        {/* <iframe className="video-player" title="vimeo-player" src="https://player.vimeo.com/video/707609150?h=5178318fd7" frameBorder="0" allowFullScreen></iframe> */}
+        <img className="home-team-poster" src={PBHPoster} alt=''/>
 
-        {/* Show bottom images */}
-        <div className="team-images">
-          <img src={Team4} className="team-image-logo" alt=''/>
-          <img src={Team5} className="team-image-logo" alt=''/>
-          <img src={Team6} className="team-image-logo" alt=''/>
+        {/* Show right images */}
+        <div className="home-team-images">
+          <img className="home-team-image-logo" src={Team4} alt=''/>
+          <img className="home-team-image-logo" src={Team5} alt=''/>
+          <img className="home-team-image-logo" src={Team6} alt=''/>
         </div>
+      </div>
+      <div className="home-body-mobile-images">
+        <img className="home-team-image-logo" src={Team1} alt=''/>
+        <img className="home-team-image-logo" src={Team2} alt=''/>
+        <img className="home-team-image-logo" src={Team3} alt=''/>
+        <img className="home-team-image-logo" src={Team4} alt=''/>
+        <img className="home-team-image-logo" src={Team5} alt=''/>
+        <img className="home-team-image-logo" src={Team6} alt=''/>
       </div>
     </div>
   );
