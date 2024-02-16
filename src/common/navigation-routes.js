a9076722ac2971f346ebc4add4f276d83c047663
Bryan Lutz <bryanlutz.212@gmail.com>
2024-02-16 13:59:47 -0700

    Adjust nav bar colors and sizing of Contact button

--- src/common/navigation-routes.js
+++ src/common/navigation-routes.js
@@ -1,8 +1,8 @@
 const NavigationLinks = [
-    { name: 'Home', route: '/'},
-    { name: 'About', route: '/About'},
-    { name: 'Teams', route: '/Teams'},
-    { name: 'Merch', route: '/Merch'},
+    { name: 'HOME', route: '/'},
+    { name: 'ABOUT', route: '/About'},
+    { name: 'TEAMS', route: '/Teams'},
+    { name: 'MERCH', route: '/Merch'},
   ]
   
   export { NavigationLinks }
