0e895d7d01136750e4deb376bb4af1b9d2b05c41
Bryan Lutz <bryanlutz.212@gmail.com>
2024-02-16 10:02:46 -0700

    Move profile pictures below descriptions

--- src/components/About/Profile/Profile.jsx
+++ src/components/About/Profile/Profile.jsx
@@ -4,14 +4,14 @@ import './Profile.scss'
 function Profile(props) {
   return (
     <div className="profile-card">
-      <div className="profile-images">
-        <img src={props.image1} className='profile-image' alt=''></img>
-        <img src={props.image2} className='profile-image' alt=''></img>
-      </div>
       <div className="profile-container">
         <span className='profile-name'>{props.name}</span>
         <span className='profile-role'>{props.role}</span>
         <span className='profile-text'>{props.text}</span>
+      </div>
+      <div className="profile-images">
+        <img src={props.image1} className='profile-image' alt=''></img>
+        <img src={props.image2} className='profile-image' alt=''></img>
       </div>
     </div>
   );
