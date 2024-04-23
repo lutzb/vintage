73226484cce95742e163cc0eb6580a8055fa9e3b
Bryan Lutz <bryanlutz.212@gmail.com>
2024-04-23 14:37:56 -0600

    Add merch products

--- src/components/Merch/Merch.jsx
+++ src/components/Merch/Merch.jsx
@@ -5,6 +5,13 @@ import NavMenu from '../NavMenu/NavMenu'
 import Footer from '../Footer/Footer'
 import MerchProduct from './MerchProduct/MerchProduct'
 
+import Koozies from '../../img/merch-koozies.png'
+import TruckerHat from '../../img/merch-hat.png'
+import ShirtGreen from '../../img/merch-shirt-green.png'
+import ShirtBlue from '../../img/merch-shirt-blue.png'
+
+const publishableKey = "pk_live_51P5WcyAWS16T0EVumVcpH5HQGZBu0QARJWih1e0enubouUlHo3p2OUaViJMGe6pKP8scouYaLaL1EXNhkU6JY0rr00KojScObc";
+
 function Merch() {
   return (
     <div className="merch">
@@ -13,14 +20,12 @@ function Merch() {
 
       {/* MERCH THINGS */}
       <div className="merch-body">
-        <p className='coming-soon-text'>MERCH COMING SOON!</p>
+        <p className='merch-header'>PBH Merch</p>
+        <MerchProduct image={Koozies} buttonId="buy_btn_1P728EAWS16T0EVujPU3dRz9" publishableKey={publishableKey} desc="PBH Neon Koozies (4)" price="$14.00 + shipping" />
+        <MerchProduct image={TruckerHat} buttonId="buy_btn_1P63MtAWS16T0EVuSJtgxqVR" publishableKey={publishableKey} desc="PBH Black Trucker Hat" price="$25.00 + shipping" />
+        <MerchProduct image={ShirtGreen} buttonId="buy_btn_1P8nSiAWS16T0EVuE2dyo5BZ" publishableKey={publishableKey} desc="PBH Logo T-Shirt (Green)" price="$28.00 + shipping" />
+        <MerchProduct image={ShirtBlue} buttonId="buy_btn_1P8nbjAWS16T0EVu0CWA39Ro" publishableKey={publishableKey} desc="PBH Logo T-Shirt (Blue)" price="$28.00 + shipping" />
       </div>
-
-      {/* Trucker Hat*/ }
-      {/* <MerchProduct buttonId="buy_btn_1P63MtAWS16T0EVuSJtgxqVR" /> */}
-
-      {/* Coozie */}
-      {/* <MerchProduct buttonId="buy_btn_1P71btAWS16T0EVus0bzeOLW" /> */}
 
       <Footer />
     </div>
--- src/components/Merch/MerchProduct/MerchProduct.jsx
+++ src/components/Merch/MerchProduct/MerchProduct.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import './MerchProduct.scss'
 
+
 function MerchProduct(props) {
   return (
-    <div className="merch-product">
-      {/* <div className='team-images'>
-        <img src={props.logo} className='team-image' alt=''></img>
-        <img src={props.image} className='team-image' alt=''></img>
+    <div className="merch-product-card">
+      <img src={props.image} className='merch-product-image' alt=''></img>
+      <div className="merch-product-detail">
+        <span className='merch-product-desc'>{props.desc}</span>
+        <span className='merch-product-desc'>{props.price}</span>
+        <div className='merch-product-buy'>
+          <stripe-buy-button  
+            buy-button-id={props.buttonId} 
+            publishable-key={props.publishableKey}
+          />
+        </div>
       </div>
-      <div className="team-desc">
-        <span className='team-name'>{props.name}</span>
-        <span className='team-text'>{props.text}</span>
-      </div> */}
-
-      <stripe-buy-button
-        buy-button-id = {props.buttonId}
-        publishable-key="pk_live_51P5WcyAWS16T0EVumVcpH5HQGZBu0QARJWih1e0enubouUlHo3p2OUaViJMGe6pKP8scouYaLaL1EXNhkU6JY0rr00KojScObc"
-      />
     </div>
   );
 }
